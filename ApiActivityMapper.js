98c78dcd2eb396c8478e4b4431d2c96b46b2c1ad
Pedro Matos de Carvalho <pedromatosdecarvalho@gmail.com>
2015-01-18 15:59:42 +0000

    added script to npm install to get bower components

--- ApiActivityMapper.js
+++ ApiActivityMapper.js
@@ -1,7 +1,10 @@
 // Objectivo: Mapear todos os dados vindos de sources diferentes para a mesma estrutura para ser mostrada da mesma forma
 module.exports = {
   Twilio: function (twilioReq) {
-   return {'source' : 'twilio', 'from' : twilioReq.Called, 'to' : twilioReq.Called , 'message' : twilioReq.CallStatus};
+   return {'source' : 'twilio',
+    'from' : twilioReq.Called,
+    'to' : twilioReq.Called ,
+    'message' : twilioReq.CallStatus};
   },
   Mail: function (gmailReq) {
    return {};
