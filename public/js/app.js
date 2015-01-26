72a930102c6a5d3a74dadc40f18c4cddbefa2c57
Luis Neves <lneves@opensoft.pt>
2015-01-26 02:36:52 +0000

    animações nas activities

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,8 @@ angular.module('hookieMonster', [
     'hookieMonster.filters',
     'hookieMonster.services',
     'hookieMonster.directives',
-    'hookieMonster.controllers'
+    'hookieMonster.controllers',
+    'ngAnimate'
 ]).
 config(['$routeProvider', function($routeProvider) {
 
