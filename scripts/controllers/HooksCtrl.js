3609eea7e1cd61f4ff2f53ea60fd8c1e5b8ab0c3
Luís Neves <lneves@opensoft.pt>
2015-03-24 18:44:02 +0000

    final checkpoint1

--- scripts/controllers/HooksCtrl.js
+++ scripts/controllers/HooksCtrl.js
@@ -5,14 +5,20 @@
 angular.module('hookieMonster')
 .controller('HooksCtrl', ['$scope', 'socketIO',
   function($scope, socketIO) {
+    
     $scope.supportedHooks = ['twilio', 'dropbox', 'trello', 'hipchat'];
-
     $scope.activities = [];
+    $scope.selectedHooks = {};
 
     $scope.supportedHooks.forEach(function(e) {
       socketIO.on(e, function(hook){
         $scope.activities.unshift(hook);
       });
+      $scope.selectedHooks[e] = DEFAULT_FILTERS_START;
     });
+
+    $scope.filterHooks = function(value, index) {
+        return value.source === undefined || $scope.selectedHooks[value.source];
+    };
 
 }]);
