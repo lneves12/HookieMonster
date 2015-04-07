4a274eb6365cf904b54f95fe64f4eba42f2c12d1
Luís Neves <lneves@opensoft.pt>
2015-04-07 13:45:55 +0100

    no message

--- scripts/controllers/DetailCtrl.js
+++ scripts/controllers/DetailCtrl.js
@@ -8,5 +8,5 @@ angular.module('hookieMonster')
     $scope.activity = $scope.ngDialogData;
 
     var cryptMail = CryptoJS.MD5($scope.activity.detail.fromMail || '');
-    $scope.gravatarURL = "http://www.gravatar.com/avatar/" + cryptMail + "?size=600&d=http%3A%2F%2Fs9.favim.com%2Forig%2F140102%2Fart-artsy-crazy-creative-Favim.com-1214952.jpg"
+    $scope.gravatarURL = "http://www.gravatar.com/avatar/" + cryptMail + "?size=400&d=http%3A%2F%2Fs9.favim.com%2Forig%2F140102%2Fart-artsy-crazy-creative-Favim.com-1214952.jpg"
 }]);
